63192515f544348490c2a75c13bc259dbb35eb92
Chris Adams <cladams0203@gmail.com>
2019-08-28 21:32:31 -0400

    lecture walkthrough

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Button, Alert, Spinner } from 'reactstrap';
+import styled from 'styled-components';
+
+const NewButton = styled.button`
+  background: blue;
+  color: white;
+  border: none;
+  ${(props) => props.size === 'small' ? 'padding: 20px' : null}
+  &:hover {
+    opacity: 0.7
+  }
+`
+const OtherBtn = styled(Button)`
+color: yellow
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NewButton size='small'>Hello</NewButton>
+    <NewButton size='medium'>Hello</NewButton>
+    <NewButton>Hello</NewButton>
+      <Button color='danger'>Danger</Button>
+      <OtherBtn>New</OtherBtn>
+      <Alert color='danger'>My alert</Alert>
+      <Spinner color='danger' size='sm'></Spinner>
     </div>
   );
 }
